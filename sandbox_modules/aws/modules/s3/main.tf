8375ef87bfb42e863bb0c5d473c81016f94fbd1c
binaryburn <contact@ecityweb.net>
2018-12-11 17:44:18 -0500

    Adding backend module.  This will be staged and not added to shim layer for now.   This will be a optional module for best practice of storing terraform state files in s3 bucket and adding statefile locking capability to Dynamodb.

--- sandbox_modules/aws/modules/s3/main.tf
+++ sandbox_modules/aws/modules/s3/main.tf
@@ -1,6 +1,27 @@
-resource "aws_s3_bucket_object" "folder1" {
-    bucket = "${aws_s3_bucket.b.id}"
+
+resource "aws_s3_bucket" "palobuckettest123" {
+  bucket = "palobucket1234"
+  acl    = "private"
+}
+
+
+resource "aws_s3_bucket_object" "palo" {
+    count   = "${length(var.s3_folders)}"
+    bucket = "${aws_s3_bucket.palobuckettest123.id}"
     acl    = "private"
-    key    = "Folder1/"
+    key    = "${var.s3_folders[count.index]}/"
     source = "/dev/null"
+}
+
+##########################################
+#### Create an S3 endpoint in the VPC ####
+##########################################
+resource "aws_vpc_endpoint" "s3" {
+  vpc_id       = "${aws_vpc.palo.id}"
+  service_name = "com.amazonaws.${var.region}.s3"
+}
+
+resource "aws_vpc_endpoint_route_table_association" "rtpalos3" {
+  vpc_endpoint_id = "${aws_vpc_endpoint.s3.id}"
+  route_table_id  = "${aws_route_table.untrust_subnet.id}"
 }

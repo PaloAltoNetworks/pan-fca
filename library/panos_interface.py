e191e18c956afe5a04b37734467a9737e5164069
acoleman@paloaltonetworks.com <acoleman@paloaltonetworks.com>
2019-01-22 14:17:35 -0600

    Update local panos_interface to support mgmt profile with DHCP fix

--- library/panos_interface.py
+++ library/panos_interface.py
@@ -322,10 +322,12 @@ def main():
     vsys_dg = module.params['vsys_dg']
     commit = module.params['commit']
     dhcp_default_route = module.params['dhcp_default_route']
+    management_profile = module.params['management_profile']
     if_name = module.params['if_name']
 
     dhcpe = ('<entry name="%s"><layer3><dhcp-client><enable>yes</enable><create-default-route>%s</create-default-route>'
-             '</dhcp-client></layer3></entry>' % (if_name,dhcp_default_route))
+             '</dhcp-client><interface-management-profile>"%s"</interface-management-profile></layer3></entry>' %
+             (if_name,dhcp_default_route,management_profile))
     dhcpx = ("/config/devices/entry[@name='localhost.localdomain']/network/interface/ethernet/entry[@name='%s']" % (if_name))
 
     # Open the connection to the PANOS device.
@@ -349,7 +351,7 @@ def main():
         module.fail_json(msg="Ethernet interfaces don't exist on Panorama")
     else:
         # Firewall
-        # Normally we should set the vsys here, but since interfaces are
+        # Normally we should set the vsys here, but since  are
         # vsys importables, we'll use organize_into_vsys() to help find and
         # cleanup when the interface is imported into an undesired vsys.
         # con.vsys = vsys_dg

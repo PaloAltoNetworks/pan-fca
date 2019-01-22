0088b1b4b455a75e4430b2c832a2d8c4464a5fd5
acoleman@paloaltonetworks.com <acoleman@paloaltonetworks.com>
2019-01-22 14:20:56 -0600

    remove "" from mgmt profile string variable for dhcpe element

--- library/panos_interface.py
+++ library/panos_interface.py
@@ -326,7 +326,7 @@ def main():
     if_name = module.params['if_name']
 
     dhcpe = ('<entry name="%s"><layer3><dhcp-client><enable>yes</enable><create-default-route>%s</create-default-route>'
-             '</dhcp-client><interface-management-profile>"%s"</interface-management-profile></layer3></entry>' %
+             '</dhcp-client><interface-management-profile>%s</interface-management-profile></layer3></entry>' %
              (if_name,dhcp_default_route,management_profile))
     dhcpx = ("/config/devices/entry[@name='localhost.localdomain']/network/interface/ethernet/entry[@name='%s']" % (if_name))
 

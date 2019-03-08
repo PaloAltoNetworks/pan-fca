5b8f95cb002f98cd1bccac549e6dd487d0bded32
acoleman@paloaltonetworks.com <acoleman@paloaltonetworks.com>
2019-03-08 16:35:10 -0600

    Start work for Azure Service Bus and Functions to enable Autoscaling

---
+++ modules/azure/function/main.tf
@@ -0,0 +1,31 @@
+resource "azurerm_resource_group" "test" {
+  name     = "azure-functions-test-rg"
+  location = "westus2"
+}
+
+resource "azurerm_storage_account" "test" {
+  name                     = "functionsapptestsa"
+  resource_group_name      = "${azurerm_resource_group.test.name}"
+  location                 = "${azurerm_resource_group.test.location}"
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_app_service_plan" "test" {
+  name                = "azure-functions-test-service-plan"
+  location            = "${azurerm_resource_group.test.location}"
+  resource_group_name = "${azurerm_resource_group.test.name}"
+
+  sku {
+    tier = "Standard"
+    size = "S1"
+  }
+}
+
+resource "azurerm_function_app" "test" {
+  name                      = "test-azure-functions"
+  location                  = "${azurerm_resource_group.test.location}"
+  resource_group_name       = "${azurerm_resource_group.test.name}"
+  app_service_plan_id       = "${azurerm_app_service_plan.test.id}"
+  storage_connection_string = "${azurerm_storage_account.test.primary_connection_string}"
+}

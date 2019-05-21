763fce23411b691516440c55c9fcb1b837890be3
Sean Youngberg <syoungberg@paloaltonetworks.com>
2019-05-21 09:23:52 -0700

    changes

---
+++ modules/aws/route_table/main.tf
@@ -0,0 +1,24 @@
+
+
+################
+# Publiс routes
+################
+resource "aws_route_table" "public" {
+  count = "${var.create_vpc && length(var.public_subnets) > 0 ? 1 : 0}"
+
+  vpc_id = "${local.vpc_id}"
+
+  tags = "${merge(map("Name", format("%s-${var.public_subnet_suffix}", var.name)), var.tags, var.public_route_table_tags)}"
+}
+
+resource "aws_route" "public_internet_gateway" {
+  count = "${var.create_vpc && length(var.public_subnets) > 0 ? 1 : 0}"
+
+  route_table_id         = "${aws_route_table.public.id}"
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = "${aws_internet_gateway.this.id}"
+
+  timeouts {
+    create = "5m"
+  }
+}

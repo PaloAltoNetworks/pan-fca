4136dcb77ebe026bec8e144ff60c545c0b1dd057
Sean Youngberg <syoungberg@paloaltonetworks.com>
2019-05-22 13:59:41 -0700

    IGW Modules

--- modules/aws/internet_gw/main.tf
+++ modules/aws/internet_gw/main.tf
@@ -2,7 +2,6 @@
 # Internet Gateway
 ###################
 resource "aws_internet_gateway" "this" {
-  count = "${var.create_vpc && length(var.public_subnets) > 0 ? 1 : 0}"
-  vpc_id = "${local.vpc_id}"
-  tags = "${merge(map("Name", format("%s", var.name)))}"
+  vpc_id            = "${var.vpc_id}"
+  tags              = "${merge(map("Name", format("%s", var.name)))}"
 }
--- modules/aws/internet_gw/outputs.tf
+++ modules/aws/internet_gw/outputs.tf
@@ -1,34 +1,4 @@
-output "vpc_id" {
-  description = "The ID of the VPC"
-  value       = "${element(concat(aws_vpc.this.*.id, list("")), 0)}"
-}
-
-output "vpc_arn" {
-  description = "The ARN of the VPC"
-  value       = "${element(concat(aws_vpc.this.*.arn, list("")), 0)}"
-}
-
-output "vpc_cidr_block" {
-  description = "The CIDR block of the VPC"
-  value       = "${element(concat(aws_vpc.this.*.cidr_block, list("")), 0)}"
-}
-
-output "vpc_instance_tenancy" {
-  description = "Tenancy of instances spin up within VPC"
-  value       = "${element(concat(aws_vpc.this.*.instance_tenancy, list("")), 0)}"
-}
-
-output "vpc_enable_dns_support" {
-  description = "Whether or not the VPC has DNS support"
-  value       = "${element(concat(aws_vpc.this.*.enable_dns_support, list("")), 0)}"
-}
-
-output "vpc_enable_dns_hostnames" {
-  description = "Whether or not the VPC has DNS hostname support"
-  value       = "${element(concat(aws_vpc.this.*.enable_dns_hostnames, list("")), 0)}"
-}
-
-output "vpc_secondary_cidr_blocks" {
-  description = "List of secondary CIDR blocks of the VPC"
-  value       = ["${aws_vpc_ipv4_cidr_block_association.this.*.cidr_block}"]
+output "igw_id" {
+  description = "Internet GW ID"
+  value       = "${element(concat(aws_internet_gateway.this.*.id, list("")), 0)}"
 }
